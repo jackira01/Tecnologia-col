c7b3394945d780bbf2438b8071ca69f3cef5c795
Jackira01 <nicolasberu27@gmail.com>
2025-05-12 20:45:08 -0500

    update logic / client

--- client/src/components/Catalogo/Catalogo.jsx
+++ client/src/components/Catalogo/Catalogo.jsx
@@ -3,6 +3,7 @@ import { useContext, useEffect } from 'react'
 import { ProductPagination } from '../Pagination/ProductPagination'
 import { Spinner } from 'flowbite-react'
 import { getProducts } from '@/services/products'
+import SideBarComponent from '../SideBar/SideBarComponent'
 const { CardComponent } = require('./CardComponent')
 
 export const Catalogo = () => {
@@ -20,7 +21,7 @@ export const Catalogo = () => {
     const fetchProducts = async () => {
       setLoaderProducts(true)
       const response = await getProducts()
-      if (response.docs.length) {
+      if (response?.docs.length) {
         setTotalPages(response.totalPages)
         setProducts(response.docs)
         setLoaderProducts(false)
@@ -39,20 +40,27 @@ export const Catalogo = () => {
   }, [setLoaderProducts, setProducts, setTotalPages])
 
   return (
-    <div className='flex flex-col justify-center items-center w-full h-screen bg-gray-900'>
-      {loaderProducts ? (
-        <Spinner />
-      ) : error ? (
-        <p className='text-white'>{error}</p>
-      ) : (
-        <div className='grid gap-10 grid-cols-1 place-items-center w-screen md:grid-cols-2 lg:grid-cols-3'>
-          {products.map(card => (
-            /* Arreglar el tamaño maximo y minimo de la card */
-            <CardComponent key={card._id} data={card} />
-          ))}
-          <ProductPagination />
-        </div>
-      )}
-    </div>
+    <>
+      <SideBarComponent className='bg-[#31363F]' />
+
+      <div className='flex flex-col justify-center items-center w-full min-h-screen bg-gray-900'>
+        {loaderProducts ? (
+          <Spinner />
+        ) : error ? (
+          <p className='text-white'>{error}</p>
+        ) : (
+          <div className='w-full my-10 px-10'>
+            <div className='mx-auto grid w-full max-w-screen-xl grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+              {products.map(data => (
+                <div key={data._id} className='h-full flex justify-center'>
+                  <CardComponent data={data} />
+                </div>
+              ))}
+            </div>
+            <ProductPagination />
+          </div>
+        )}
+      </div>
+    </>
   )
 }
